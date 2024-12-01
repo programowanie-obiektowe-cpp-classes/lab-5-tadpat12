c94a3112259eb31b946b8bdd45891c48aedaa592
tadpat12 <tadpat12@gmail.com>
2024-12-01 21:29:14 +0100

    1

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,17 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
+    std::vector< char > char_vector;
     // Twoja implementacja tutaj
-    return {};
+    //return {};
+    for(auto iter= people.rbegin(); iter != people.rend(); iter++){
+        iter -> birthday();
+        if(iter ->isMonster() == true){
+            char_vector.push_back('n');
+        }
+        else if(iter -> isMonster() == false){
+            char_vector.push_back('y');
+        }
+    }
+    return char_vector;
 }
